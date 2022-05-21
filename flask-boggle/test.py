81db3c3cc293e3caa7bbce95f5f59b3511c55a03
MITHIRI1 <87658049+MITHIRI1@users.noreply.github.com>
2022-05-21 17:48:18 -0400

    Update test.py

--- flask-boggle/test.py
+++ flask-boggle/test.py
@@ -4,7 +4,66 @@ from flask import session
 from boggle import Boggle
 
 
+app.config['TESTING'] = True
+
+
+
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def setUp(self):
+        
+        self.client = app.test_client()
+        app.config['TESTING'] = True
 
+
+    def test_homepage(self):
+        
+
+        with self.client:
+            response = self.client.get('/new')
+            self.assertIn('board', session)
+            self.assertIn('num_plays', session)
+            self.assertIsNone(session.get('highscore'))
+            self.assertEqual(response.status_code, 302)
+            
+        
+        with self.client:
+            response = self.client.get('/')
+            self.assertIn('board', session)
+            self.assertIn('num_plays', session)
+            self.assertIsNone(session.get('highscore'))
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'High Score:', response.data)
+            self.assertIn(b'<div id="game">', response.data)
+            self.assertIn(b'Score:', response.data)
+
+
+    def test_valid_word(self):
+        
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess['board'] = [["C", "A", "T", "T", "T"],
+                                ["C", "A", "T", "T", "T"],
+                                ["C", "A", "T", "T", "T"],
+                                ["C", "A", "T", "T", "T"],
+                                ["C", "A", "T", "T", "T"],]
+
+            response = self.client.get('/check-guess?word=cat')
+            self.assertEqual(response.json['result'], 'ok')
+            self.assertEqual(response.status_code, 200)
+    
+
+    def test_invalid_word(self):
+        
+        self.client.get("/new")
+        response = self.client.get('/check-guess?word=impossible')
+        self.assertEqual(response.json['result'], 'not-on-board')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_not_a_word(self):
+        
+        self.client.get("/new")
+        response = self.client.get('/check-guess?word=dsafd')
+        self.assertEqual(response.json['result'], 'not-word')
+        self.assertEqual(response.status_code, 200)
